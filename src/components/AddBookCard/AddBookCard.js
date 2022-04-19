23e9e73ab286afca50b0eb2c682bbd928fe60ce7
MusicDev33 <smccowan33@gmail.com>
2022-04-18 22:56:44 -0700

    recursion problems with this

--- src/components/AddBookCard/AddBookCard.js
+++ src/components/AddBookCard/AddBookCard.js
@@ -8,13 +8,19 @@ import Container from 'react-bootstrap/Container';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 
+import { IconContext } from 'react-icons';
+import { FaRegCheckCircle, FaCheckCircle } from 'react-icons/fa';
 
 export const AddBookCard = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const [pages, setPages] = useState('');
+  const [isRead, setIsRead] = useState(false);
+  const [checkClass, setCheckClass] = useState('check');
+  const [checkIcon, setCheckIcon] = useState((<FaRegCheckCircle onClick={() => setIsRead(!isRead)} />));
 
   return (
+
     <div className="add-book-card">
       <Container className="py-1 px-3">
         <Row>
@@ -29,7 +35,12 @@ export const AddBookCard = () => {
         </Row>
         <Row>
           <Col sm={3}>
-            <FormControl inputmode="numeric" placeholder="Pages" className="card-input" value={pages} onChange={e => setPages(e.target.value)} />
+            <FormControl inputMode="numeric" placeholder="Pages" className="card-input" value={pages} onChange={e => setPages(e.target.value)} />
+          </Col>
+          <Col className="pt-1">
+            <IconContext.Provider value={{ className: checkClass }}>
+              {checkIcon}
+            </IconContext.Provider>
           </Col>
           <Col className="text-end pt-2">
             <Button className="md-btn-1">Submit</Button>
