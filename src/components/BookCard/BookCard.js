2887f6bc408ecdde0ee93b5da28874f8267ec1fc
MusicDev33 <smccowan33@gmail.com>
2022-04-29 00:48:37 -0700

    BookCard keeps track of read data (code quality is low)

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+import { getReadByBookId, createRead, setReadPagesById } from 'services/read.service';
 
 import './BookCard.scss';
 
@@ -8,6 +10,37 @@ import Container from 'react-bootstrap/Container';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
 export const BookCard = ({book}) => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [read, setRead] = useState();
+  const [currentPageChanged, setCurrentPageChanged] = useState(false);
+
+  const getRead = async () => {
+    const readData = await getReadByBookId(book._id);
+    console.log(readData);
+    if (readData.success) {
+      setRead(readData.data);
+      setCurrentPage(readData.data.currentPage);
+    }
+  }
+
+  const setReadPages = async (currentPage) => {
+    if (!read) {
+      const res = await createRead(book._id, currentPage);
+      console.log(res);
+    } else {
+      const res = await setReadPagesById(book._id, currentPage);
+      console.log(res);
+    }
+  }
+
+  const getPercentage = (currentPage) => {
+    const percent = (currentPage / book.pages) * 100;
+    return currentPage ? percent : 0;
+  }
+
+  useEffect(() => {
+    getRead();
+  }, []);
 
   return (
     <div className="book-card">
@@ -24,14 +57,27 @@ export const BookCard = ({book}) => {
         </Row>
         <Row className="pb-0 pt-2">
           <Col>
-            <ProgressBar now={60} className="progress" label={`${60}%`} />
+            <ProgressBar now={getPercentage(currentPage)} className="progress" />
           </Col>
         </Row>
         <Row className="mt-2">
           <Col>
             <div className="pages text-center">
-              <input className="pages-input" /> /{book.pages} pages
-              </div>
+              <input className="pages-input" 
+                value={currentPage} 
+                onChange={e => {
+                  setCurrentPage(e.target.value);
+                  setCurrentPageChanged(true);
+                }}
+                onBlur={() => {
+                  console.log('blur');
+                  if (currentPageChanged) {
+                    setReadPages(currentPage);
+                    setCurrentPageChanged(false);
+                  }
+                }}
+              /> /{book.pages} pages
+            </div>
           </Col>
         </Row>
       </Container>
